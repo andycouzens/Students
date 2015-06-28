21bd68e3705aeff59f726ee4762bd65aad2be21a
Andy Couzens <andy.couzens@gmail.com>
2015-06-28 22:38:26 +0100

    Added missing files

---
+++ Students.Shell/ViewModels/EnrolmentListViewModel.cs
@@ -0,0 +1,55 @@
+﻿using Students.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Students.Shell
+{
+    public class EnrolmentListViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Enrolment> _enrolments;
+        private EnrolmentRepo _repo;
+
+        public EnrolmentListViewModel()
+        {
+        }
+
+        public async void LoadEnrolments()
+        {
+            if (DesignerProperties.GetIsInDesignMode(
+                new System.Windows.DependencyObject())) return;
+
+            _repo = new EnrolmentRepo();
+            Enrolments = new ObservableCollection<Enrolment>(await _repo.GetEnrolmentsAsync());
+        }
+
+        public ObservableCollection<Enrolment> Enrolments
+        {
+            get
+            {
+                return _enrolments;
+            }
+            set
+            {
+                if (_enrolments != value)
+                {
+                    _enrolments = value;
+                    OnPropertyChanged("Enrolments");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
